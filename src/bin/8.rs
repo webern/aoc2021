9c031cb0d48f15fed71977bfafcc11d7baacfcf7
Matt Briggs <matthew.james.briggs@gmail.com>
2021-12-08 12:06:42 -0800

    bloop

---
+++ src/bin/8.rs
@@ -0,0 +1,5 @@
+use aoc2021::day8::solve;
+
+fn main() {
+    solve();
+}
---
+++ src/day8/mod.rs
@@ -0,0 +1,20 @@
+/*!
+
+!*/
+
+mod data;
+
+use crate::day8::data::INPUT_DATA;
+
+pub fn solve() {
+    println!("Part 1: {}", solve_part_1(&INPUT_DATA));
+    println!("Part 2: {}", solve_part_2(&INPUT_DATA));
+}
+
+fn solve_part_1(_input: &[usize]) -> usize {
+    todo!()
+}
+
+fn solve_part_2(_input: &[usize]) -> usize {
+    todo!()
+}
